1589c48906a36cb7a97ebc7b354015b0629dd56c
Cammie <inazuma261@gmail.com>
2018-12-02 14:47:41 -0500

    Almost forgot to update that

--- src/main/java/com/camellias/resizer/Reference.java
+++ src/main/java/com/camellias/resizer/Reference.java
@@ -7,7 +7,7 @@ public class Reference
 	public static final String NAME = "Resizing Potion";
 	public static final String VERSION = "1.1";
 	public static final String ACCEPTEDVERSIONS = "[1.12.2]";
-	public static final String DEPENDENCIES = "required-after:forge@[14.23.4.2759,)";
+	public static final String DEPENDENCIES = "required-after:forge@[14.23.5.2795,)";
 		
 	//Proxies
 	public static final String CLIENT_PROXY_CLASS = "com.camellias.resizer.proxy.ClientProxy";
